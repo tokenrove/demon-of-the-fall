7f87578cb0de770ae5fb3b0098b33a43a117c1d3
Julian Squires <julian@cipht.net>
2015-02-07 00:00:34 -0500

    Get it compiling

--- src/main.lisp
+++ src/main.lisp
@@ -6,21 +6,13 @@
 
 (in-package :demon-of-the-fall)
 
-(defun first-init ()
-  "Called when we first start."
-  (fetus:create-display :scale 1)
-  (fetus:event-init)
-  (fetus:font-init)
-  (equinox:initialize-actor-data)
-  (equinox:initialize-room-data))
-
-(defun final-shutdown ()
-  (fetus:event-shutdown)
-  (fetus:destroy-display))
-
-
 (defvar *camera-follow*)
 
+(defmacro with-everything (&body body)
+  `(fetus:with-display (:scale 3)
+     (equinox:initialize-actor-data)
+     (equinox:initialize-room-data)
+     ,@body))
 
 (defun in-game-loop (starting-room)
   "Interactive game-loop on the current display, starting from the
@@ -43,33 +35,34 @@ given ROOM.  Note that the display must already have been created."
 		 sprite-manager))
 
 	  (loop
-	   (fetus:timer-start-frame 20)
-	   (fetus:event-update)
-	   (when (fetus:event-pressedp fetus:+ev-quit+)
-	     (return))
+            (fetus:timer-start-frame 20)
+            (fetus:clear-display)
+            (fetus:event-update)
+            (when (fetus:event-pressedp fetus:+ev-quit+)
+              (return))
 
-	   ;; XXX: ugly hack.
-	   (multiple-value-bind (destination point)
-	       (check-room-change *camera-follow*)
-	     (when destination
-	       (fetus:destroy-sprite-manager sprite-manager)
-	       (setf sprite-manager (fetus:create-sprite-manager
-				     #'equinox:isometric-sprite-cmp))
-	       (equinox:create-actor-manager)
-	       (setf room (load-room destination sprite-manager))
-	       (setf *camera-follow*
-		     (spawn-actor :peter point sprite-manager))))
+            ;; XXX: ugly hack.
+            (multiple-value-bind (destination point)
+                (check-room-change *camera-follow*)
+              (when destination
+                (fetus:destroy-sprite-manager sprite-manager)
+                (setf sprite-manager (fetus:create-sprite-manager
+                                      #'equinox:isometric-sprite-cmp))
+                (equinox:create-actor-manager)
+                (setf room (load-room destination sprite-manager))
+                (setf *camera-follow*
+                      (spawn-actor :peter point sprite-manager))))
 
-	   (equinox:update-camera *camera-follow*)	   
-	   (equinox:update-actors room)
-	   (equinox:redraw room)
-	   (fetus:update-all-sprites sprite-manager)
+            (equinox:update-camera *camera-follow*)
+            (equinox:update-actors room)
+            (equinox:redraw room)
+            (fetus:update-all-sprites sprite-manager)
 
-	   ;;(paint-osd font)
-	   (fetus:refresh-display)
+            ;;(paint-osd font)
+            (fetus:present-display)
 
-	   (fetus:timer-end-frame)
-	   (incf (car fps-count)))
+            (fetus:timer-end-frame)
+            (incf (car fps-count)))
 	  (format t "~&Frames-per-second: ~D"
 		  (float (* 1000 (/ (car fps-count) (- (fetus:timer-get-ticks)
 						       (cdr fps-count)))))))))))
